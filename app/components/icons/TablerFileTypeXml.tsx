a371de5a2ae1a5c164ac5adef822e0fe9ba1d1da
Danushka-Madushan <danushkamadushan128@gmail.com>
2025-11-15 11:07:55 +0530

    feat: create TablerFileTypeXml.tsx

---
+++ app/components/icons/TablerFileTypeXml.tsx
@@ -0,0 +1,8 @@
+import React, { SVGProps } from 'react'
+
+export function TablerFileTypeXml(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}>{/* Icon from Tabler Icons by Paweł Kuna - https://github.com/tabler/tabler-icons/blob/master/LICENSE */}<g fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"><path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M5 12V5a2 2 0 0 1 2-2h7l5 5v4M4 15l4 6m-4 0l4-6m11 0v6h3m-11 0v-6l2.5 3l2.5-3v6" /></g></svg>
+  )
+}
+export default TablerFileTypeXml
